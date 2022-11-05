1b36c22f47ff2506f1352ac98513e70750e9382c
Mickael Kurmann <m@kurmann.io>
2022-11-05 18:17:29 +0100

    test all models

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
-  it 'just works...' do
-    expec(Fabricate(:work)).to be_valid
+  it 'creating user work' do
+    expect(Fabricate(:user).class).to be(User) 
+  end
+
+  it 'creating task work' do
+    expect(Fabricate(:task).class).to be(Task) 
+  end
+
+  it 'creating work that as tasks that have users dont work' do
+    expect(Fabricate(:work)).to be_valid
   end
 end
